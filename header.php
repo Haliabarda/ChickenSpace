441832c38d69abd4e97fc98a77ccd2f112937633
Haliabarda <113632516+Haliabarda@users.noreply.github.com>
2023-06-19 11:51:10 +0300

    database connection

---
+++ header.php
@@ -0,0 +1,20 @@
+<header class="p-3"style="background-color: #0a0a2a">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-3 text-secondary" style="font-size: 24px;">Головна</a></li>
+                <li><a href="#" class="nav-link px-2 text-white" style="font-size: 24px;">Новини</a></li>
+                <li><a href="#" class="nav-link px-2 text-white" style="font-size: 24px;">Меню</a></li>
+                <li><a href="#" class="nav-link px-2 text-white" style="font-size: 24px;">Категорії</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                <input type="search" class="form-control form-control-dark text-bg-dark" style="padding: 10px;" placeholder="Search..." aria-label="Search">
+            </form>
+
+            <div class="text-end">
+                <button type="button" class="btn btn-warning" style="width: 100px; height: 40px; font-size: 19px;">Sign-up</button>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>ChickenSpace</title>
+</head>
+<body>
+<?php
+include("header.php");
+?>
+<div class="greeting">
+    <h1>Ласкаво просимо до ChickenSpace!</h1>
+    <p>Насолоджуйтесь нашими смачними стравами з курки.</p>
+</div>
+</body>
+</html>
