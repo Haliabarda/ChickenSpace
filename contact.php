690c7affae987fe2ca6c446b3094358c9f2675e5
Chrystyna Haliabarda <galyabarda1998@gmai.com>
2023-06-23 13:12:04 +0300

    Contact page

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>ChickenSpace</title>
+</head>
+<body>
+<?php
+include("head/header.php");
+?>
+<div class="greet">
+    <h1>Для замовлення звертайтеся за номером: +380990050171</h1>
+    <p>Ви можете нас знайти в смт. Гусятин, Тернопільська область, за адресою: вул. Шевченка, 15</p>
+</div>
+</body>
+</html>
